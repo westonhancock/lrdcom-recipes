8e262b95cbc16b651a18c920050a5ef4b544b1c9
Rebecca Dai <rebecca.dai@liferay.com>
2016-02-02 18:16:18 -0800

    add popup instantiation notes

---
+++ 6.2.x/js-instantiation/popup.js
@@ -0,0 +1,36 @@
+Basic popup instantiation:
+
+	<div class="pop-up">
+		<btn class="btn pop-up-trigger">Trigger</btn>
+		<div class="pop-up-content">Content that I want copied to the popup</div>
+	</div>
+
+	<script>
+		AUI().ready(
+		'pop-up',
+		function(A) {
+			new A.PopUp().render();
+		}
+	);
+	</script>
+
+Custom class names:
+
+	<div class="video-pop-up">
+		<btn class="btn video-pop-up-trigger">Trigger</btn>
+		<div class="video-pop-up-content">Content that I want copied to the popup</div>
+	</div>
+
+	<script>
+		AUI().ready(
+		'pop-up',
+		function(A) {
+			new A.PopUp(
+				{
+					baseClassName: 'video-pop-up',
+					trigger: '.video-pop-up-trigger'
+				}
+			).render();
+		}
+	);
+	</script>
