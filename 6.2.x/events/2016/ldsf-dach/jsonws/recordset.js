71877c3ea6a23abf115f8009d0511559381832ee
Phillip_Chan <phillip.chan@liferay.COM>
2016-02-19 15:59:10 -0800

    adding 2016 events

---
+++ 6.2.x/events/2016/ldsf-dach/jsonws/recordset.js
@@ -0,0 +1,81 @@
+[{
+  "DDMStructureId": 67579943,
+  "companyId": 1,
+  "createDate": 1452634906000,
+  "description": "France Symposium Agenda List",
+  "descriptionCurrentValue": "France Symposium Agenda List",
+  "groupId": 67510365,
+  "minDisplayRows": 20,
+  "modifiedDate": 1453950625000,
+  "name": "France Symposium | Agenda",
+  "nameCurrentValue": "France Symposium | Agenda",
+  "recordSetId": 67586499,
+  "recordSetKey": "67586498",
+  "scope": 0,
+  "userId": 31522702,
+  "userName": "Luke Shackelford",
+  "uuid": "30f23d63-9941-4afc-8a4d-84524c97d48d"
+}
+,
+{
+	  "DDMStructureId": 69365905,
+	  "companyId": 1,
+	  "createDate": 1454684648000,
+	  "description": "Agenda for LDSF DACH 2016",
+	  "descriptionCurrentValue": "Agenda for LDSF DACH 2016",
+	  "groupId": 67510365,
+	  "minDisplayRows": 20,
+	  "modifiedDate": 1454684648000,
+	  "name": "LDSF-DACH | Agenda",
+	  "nameCurrentValue": "LDSF-DACH | Agenda",
+	  "recordSetId": 69365982,
+	  "recordSetKey": "69365981",
+	  "scope": 0,
+	  "userId": 66748356,
+	  "userName": "Allen Ziegenfus",
+	  "uuid": "c004d6a3-b622-4a9f-888d-cd9b1fb82fa9"
+	}	
+
+
+,
+
+{
+  "DDMStructureId": 67579943,
+  "companyId": 1,
+  "createDate": 1452634906000,
+  "description": "<?xml version='1.0' encoding='UTF-8'?><root available-locales=\"en_US\" default-locale=\"en_US\"><Description language-id=\"en_US\">France Symposium Agenda List<\/Description><\/root>",
+  "descriptionCurrentValue": "France Symposium Agenda List",
+  "groupId": 67510365,
+  "minDisplayRows": 20,
+  "modifiedDate": 1453950625000,
+  "name": "<?xml version='1.0' encoding='UTF-8'?><root available-locales=\"en_US\" default-locale=\"en_US\"><Name language-id=\"en_US\">France Symposium | Agenda<\/Name><\/root>",
+  "nameCurrentValue": "France Symposium | Agenda",
+  "recordSetId": 67586499,
+  "recordSetKey": "67586498",
+  "scope": 0,
+  "userId": 31522702,
+  "userName": "Luke Shackelford",
+  "uuid": "30f23d63-9941-4afc-8a4d-84524c97d48d"
+}
+
+,
+
+{
+	  "DDMStructureId": 69365905,
+	  "companyId": 1,
+	  "createDate": 1454684648000,
+	  "description": "<?xml version='1.0' encoding='UTF-8'?><root available-locales=\"en_US\" default-locale=\"en_US\"><Description language-id=\"en_US\">Agenda for LDSF DACH 2016<\/Description><\/root>",
+	  "descriptionCurrentValue": "Agenda for LDSF DACH 2016",
+	  "groupId": 67510365,
+	  "minDisplayRows": 20,
+	  "modifiedDate": 1454684648000,
+	  "name": "<?xml version='1.0' encoding='UTF-8'?><root available-locales=\"en_US\" default-locale=\"en_US\"><Name language-id=\"en_US\">LDSF-DACH | Agenda<\/Name><\/root>",
+	  "nameCurrentValue": "LDSF-DACH | Agenda",
+	  "recordSetId": 69365982,
+	  "recordSetKey": "69365981",
+	  "scope": 0,
+	  "userId": 66748356,
+	  "userName": "Allen Ziegenfus",
+	  "uuid": "c004d6a3-b622-4a9f-888d-cd9b1fb82fa9"
+	}
+]
