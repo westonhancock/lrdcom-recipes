5a73524f67c68fd597d5b1c75b9bcb91abce6692
Phillip_Chan <phillip.chan@liferay.COM>
2016-05-13 14:10:37 -0700

    CSV Hubspot Tool: Updated app instructions for user

--- 6.2.x/csv-to-hubspot/src/js/modules/steps/step2/view.js
+++ 6.2.x/csv-to-hubspot/src/js/modules/steps/step2/view.js
@@ -1,3 +1,3 @@
 'use strict';
 step2view = Object.create(null);
-step2view['view.html'] = '<div class="page step2" data-step="2">\n	<div class="centered">\n		<h2>Step 2</h2>\n		<p>Tell me about the event and campaign.</p>\n		<form>\n			<div class="form-group">\n				<label>Campaign</label>	\n				<select class="campaign-list-dropdown">\n					\n				</select>\n			</div>\n		</form>\n		\n	</div>\n</div>';
+step2view['view.html'] = '<div class="page step2" data-step="2">\n	<div class="centered">\n		<p><strong>Step 4 (Optional): </strong>. Were the interactions your list describes tied to a Salesforce campaign? If so, select a campaign from the list below. If not, leave this field blank and continue.</p>\n		<p>Need to create a Salesforce campaign? <a href="mailto:karena.kenmotsu@liferay.com">Contact Karena</a></p>\n		<form>\n			<div class="form-group">\n				<label>Campaign</label>	\n				<select class="campaign-list-dropdown">\n					\n				</select>\n			</div>\n		</form>\n		\n	</div>\n</div>';
