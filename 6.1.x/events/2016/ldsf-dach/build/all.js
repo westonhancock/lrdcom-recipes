5912feafda2c114bcaf3523185a0ca54d21fe9cf
allen-ziegenfus <allen.ziegenfus@liferay.com>
2016-07-18 09:39:26 +0200

    hide portlet success message

---
+++ 6.1.x/events/2016/ldsf-dach/build/all.js
@@ -0,0 +1,42 @@
+<script>
+AUI().use('panel', function (A) {
+
+	var panel = new A.Panel({
+		srcNode: '#recap-popup',
+		zIndex: 9999,
+		id: "recappanel",
+		centered: true,
+		/* hey buddy, we are going to stay in the viewport */
+		constrain: true,
+		modal: true,
+		visible: false,
+		render: true,
+		headerContent: "Events Newsletter",
+		buttons: [
+			{
+                value: " ",
+                action: function (e) {
+                    e.preventDefault();
+                    panel.hide();
+                },
+                section: A.WidgetStdMod.HEADER,
+				classNames: "close-popup-content"
+			}
+		],
+		hideOn: [
+            {
+				eventName: 'clickoutside'
+            }]
+	});
+	A.one('window').on(
+		'resize',
+		function () {
+			panel.centered = true;
+		}
+	);
+
+	A.all('.register-button .btn').on('click', function () {
+        panel.show();
+    });
+});
+</script>
