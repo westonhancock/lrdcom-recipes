15c14a2588fb93509bf34ffe4bc86a4f36a9deaf
allen-ziegenfus <allen.ziegenfus@liferay.com>
2016-08-25 13:31:02 +0200

    check in devcon screenshots

---
+++ 6.1.x/events/2016/devcon/test/devcon.js
@@ -0,0 +1,60 @@
+var sizes = [
+    { width: 320, height: 640 },
+    { width: 375, height: 667 },
+    { width: 768, height: 1024 },
+    { width: 320, height: 568 },
+    { width: 414, height: 736 },
+    { width: 375, height: 667 },
+    { width: 1366, height: 768 },
+    { width: 1920, height: 1080 },
+    { width: 1600, height: 900 },
+    { width: 1440, height: 900 },
+    { width: 1280, height: 1024 }
+];
+
+var url = "http://www.liferay.com/solutionsforum-de";
+var url = "http://localhost:3000";
+var url = "http://www.liferay.com/web/events2016/austria";
+var url = "http://www.liferay.com/web/events2016/devcon";
+var pageName = "devcon";
+var desktopSafari="User-Agent	Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7";
+
+function savePage(size, filename) {
+    var page = require('webpage').create();
+  //  page.settings.userAgent = desktopSafari;
+    page.onConsoleMessage = function (msg) {
+    console.log(msg);
+}
+
+    page.onResourceError = function (resourceError) {
+        console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+        console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+    };
+
+
+    page.viewportSize = { width: size.width, height: size.height };
+    page.open(url, function start(status) {
+        console.log("opening url for viewport size: " + size.width + " x" + size.height);
+
+
+        setTimeout(function () {
+
+
+            var htmlClasses = page.evaluate(function() {
+                    return document.querySelector("html").getAttribute("class");
+            });
+            console.log("HTML Classes: " + htmlClasses);
+            page.render(filename);
+            console.log("rendering url for viewport size: " + size.width + " x" + size.height);
+        }, 20000);
+
+    });
+}
+
+for (var i = 0; i < sizes.length; i++) {
+    var value = sizes[i];
+    //console.log(value.width);
+
+    savePage(value, "test/" + pageName + "-" + value.width + "x" + value.height + ".png");
+}
+
